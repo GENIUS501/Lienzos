e8312717ef65096bc2ad6819b55d68d29c6d3410
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2023-04-25 10:07:26 -0600

    Subida inicial

---
+++ Entidades/EClientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class EClientes
+    {
+        public int ID_Cliente { get; set; }
+        public string Nombre { get; set; }
+        public string Primer_Apellido { get; set; }
+        public string Segundo_Apellido { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public int Genero { get; set; }
+    }
+}
---
+++ Entidades/EProductos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class EProductos
+    {
+        public int ID_Producto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int ID_Tipo_Producto { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
